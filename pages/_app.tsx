399fd675208794480ffda43a53d1164ceb8fbac5
LeSavantR <ruben_hernandez1993@hotmail.es>
2022-12-22 19:02:39 -0500

    Final Tutorial.... :(

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
+import { createContext } from 'react'
+
+const ThemeContext = createContext('dark')
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <>
-    <Component {...pageProps} />
-  </>
+  return (
+    <ThemeContext.Provider value='dark'>
+      <Component {...pageProps} />
+    </ThemeContext.Provider>
+  )
 }
